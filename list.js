1a2def769cd6d83e6f0264d0f0bb0967085c8a0d
sjay <saira.jamil64@gmail.com>
2019-05-13 01:39:46 +0200

    first- phase -- changes in the old code

--- list.js
+++ list.js
@@ -3,35 +3,37 @@ window.onload = function () {
 
     paper.rect(0, 0, 1000, 1000);
 
+    let result = {
 
-    "use strict";
-    let result ;
-    function clicked() {
-        result = document.getElementById('data').value;
+        fieldValue: null,
+        rangeA : null,
+         rangeB : null ,
 
-            var mouth = paper.rect(20, 160, result * 5, 200)
-                .attr({"fill": "yellow"}).animate({"transform": "..r-90, 200,200"});
+        animation() {
+
+            this.rangeA = paper.rect(20, 160, this.fieldValue * 5, 200)
+
+                .attr({"fill": "yellow"}).animate({"transform": "..r-90, 200,200"}, 5000, "elastic") ;
 
 
+            // setTimeout(this.rangeA.hide(), 5000);
 
+        }
 
     }
-    document.getElementById('btn').addEventListener('click', clicked);
+
 
     let inputField = document.getElementById('data');
     inputField.addEventListener('input', function myFunction() {
 
-       let  result = document.getElementById('data').value;
+        result.fieldValue = document.getElementById('data').value;
+        console.log(result.fieldValue);
         // let rect = paper.rect(0,  100,  result * 5 ,40 ).attr({fill: "0-#fff-#f00:20-#000", "stroke-width": 2, stroke: "red"})
         // rect.hide();
- if (result < 10)
-        var mouth = paper.rect(20, 160, result * 5, 200)
-            .attr({"fill": "yellow"}).animate({"transform": "..r-90, 200,200"}, 5000, "elastic");
+        if (result.fieldValue) {
 
-
- else
-     var mouth = paper.rect(20, 160, result * 5, 200)
-         .attr({"fill": "0-#fff-#f08:20-#000"}).animate({"transform": "..r-90, 200,200"}, 5000, "elastic");
+            result.animation();
+        }
 
 
     })
