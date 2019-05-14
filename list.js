ed7903494813f577ec8fac8d2e69961e89d19ffb
sjay <saira.jamil64@gmail.com>
2019-05-14 03:15:00 +0200

    secondt- phase -- bars displaced

--- list.js
+++ list.js
@@ -1,22 +1,36 @@
 window.onload = function () {
-    let paper = Raphael('paper', 1000, 1000);
+    let paper = Raphael('paper', 800, 500);
 
-    paper.rect(0, 0, 1000, 1000);
+   // paper.rect(0, 0, 800, 500);
 
     let result = {
-        x:20,
-        y:160,
-        fieldValue: null,
+
+        height: null,
         rangeA : null,
-         rangeB : null ,
+         rangeB : 5 ,
+        distance: 0 ,
 
-        animation( ) {
 
-            this.rangeA = paper.rect(this.x , this.y, this.fieldValue * 5, 200)
 
-                .attr({"fill": "yellow"}).animate({"transform": "..r-90, 200,200"}, 5000, "elastic");
 
-            
+        animation() {
+
+            let x = 0;
+
+                x = parseInt(x + this.height + this.rangeB);
+                console.log(x);
+
+
+            this.rangeA = paper.rect(x  , 50, 30 ,this.height * 10  )
+
+
+                .attr({"fill": "green"}).animate({"transform": "..r-180, 400,250"}, 5000, "elastic");
+
+
+
+
+
+
 
         }
 
@@ -35,11 +49,11 @@ window.onload = function () {
     let inputField = document.getElementById('data');
     inputField.addEventListener('input', function myFunction() {
 
-        result.fieldValue = document.getElementById('data').value;
-        console.log(result.fieldValue);
-        // let rect = paper.rect(0,  100,  result * 5 ,40 ).attr({fill: "0-#fff-#f00:20-#000", "stroke-width": 2, stroke: "red"})
+        result.height = document.getElementById('data').value;
+        console.log(result.height);
+        // let rect = paper.rect(0,  100,  result * 5 ,40 ).attr({fill: "0-#fff-#f00:20-#000", "stroke-height": 2, stroke: "red"})
         // rect.hide();
-        if (result.fieldValue) {
+        if (result.height) {
 
             result.animation();
 
